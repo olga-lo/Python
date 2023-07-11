2df3c16f76d8a874bf273fda48cf8434db008bc9
olga-lo <139242247+olga-lo@users.noreply.github.com>
2023-07-11 20:18:49 +0500

    Create print.py

---
+++ print.py
@@ -0,0 +1,14 @@
+#функция print()
+
+print("Hello world")
+
+print('Python')
+
+print('скоро я', 'буду', 'программировать', 'на языке', 'python')
+
+print('Как тебя зовут?')
+name=input()
+print('Привет,', name)
+
+name = input('Как тебя зовут?')
+print('Привет,', name)
